7c4cc1f2efeb7c2aaad6c81094eeaa797ada77ec
Simon J Stuart <lakraven@gmail.com>
2026-09-11 22:30:33 +0200

    Reset persistent result accounting on rollback

--- src/detail/ESPressio_CommandPersistence_Lifecycle.hpp
+++ src/detail/ESPressio_CommandPersistence_Lifecycle.hpp
@@ -6,6 +6,11 @@ void CommandExecutionLedger<T>::RollbackInitialization() noexcept {
     if(!_initialized) return;
     _pending.fill({});
     _generations.fill(0);
+    _maximumResultPayloadBytes=0;
+    _retainedResultCount=0;
+    _retainedResultBytes=0;
+    _reservedResultCount=0;
+    _reservedResultBytes=0;
     _initialized=false;
 }
 }
